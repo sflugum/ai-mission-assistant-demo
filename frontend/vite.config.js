3b86588ca62284bbee6e91bdf885f00283da2abf
Silas Flugum <siclops.ch@gmail.com>
2026-04-17 09:21:36 -0400

    add strict local host for clone

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
+    strictPort: true,
+
     // Development convenience: avoid CORS by proxying to the Express backend.
     proxy: {
       '/analyze': {
