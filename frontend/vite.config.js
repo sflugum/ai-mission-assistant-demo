f77eb8949bc0cca0cc541a3cddb1680234dfe539
Silas Flugum <siclops.ch@gmail.com>
2026-07-05 22:29:43 -0400

    fix: add sentry dependency to frontend folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import sentryVitePlugin from "@sentry/vite-plugin"
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
